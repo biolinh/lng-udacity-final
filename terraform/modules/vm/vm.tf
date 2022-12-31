329252ba0496c1c74971872184b778ad1f94d985
Linh Ng (biolinh) <linh.bio@gmail.com>
2022-12-31 04:37:12 -0800

    update vm

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,35 +1,25 @@
 resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+  name                = "${var.application_type}-${var.resource_type}-vnet"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = "${var.public_ip_address_id}"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+  name                = "${var.application_type}-${var.resource_type}-vm"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
   size                = "Standard_DS2_v2"
-  admin_username      = ""
-  network_interface_ids = []
-  admin_ssh_key {
-    username   = ""
-    public_key = "file("~/.ssh/id_rsa.pub")"
-  }
+  admin_username      = "${var.admin_username}"
+  admin_password      = "${var.vm_password}"
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
   }
 }
